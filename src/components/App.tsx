fd0f1c9d2f6f66b82ac34db15055def9f84a6145
Ositha Dewruwan <dewruwanebay@gmail.com>
2025-01-10 06:50:12 +0530

    created all functionalities

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,51 @@
+import { useState } from "react";
 import Background from "./Background";
 import Container from "./Container";
 import Footer from "./Footer";
-import Header from "./Header";
+import Header, { HeaderTop } from "./Header";
+import Logo from "./Logo";
+import BookmarksButton from "./BookmarksButton";
+import SearchForm from "./SearchForm";
+import Sidebar, { SidebarTop } from "./Sidebar";
+import JobItemContent from "./JobItemContent";
+import ResultsCount from "./ResultsCount";
+import SortingControls from "./SortingControls";
+import PaginationControls from "./PaginationControls";
+import JobList from "./JobList";
+import { useJobItems } from "../lib/hooks";
 
 function App() {
+  const [searchText, setSearchText] = useState("");
+  const { jobItemsSliced, isLoading, totalNumberOfResults } =
+    useJobItems(searchText);
+
   return (
     <>
       <Background />
-      <Header />
-      <Container />
+      <Header>
+        <HeaderTop>
+          <Logo />
+          <BookmarksButton />
+        </HeaderTop>
+
+        <SearchForm searchText={searchText} setSearchText={setSearchText} />
+      </Header>
+
+      <Container>
+        <Sidebar>
+          <SidebarTop>
+            <ResultsCount totalNumberOfResults={totalNumberOfResults} />
+            <SortingControls />
+          </SidebarTop>
+
+          <JobList jobItems={jobItemsSliced} isLoading={isLoading} />
+
+          <PaginationControls />
+        </Sidebar>
+
+        <JobItemContent />
+      </Container>
+
       <Footer />
     </>
   );
